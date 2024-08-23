fa7821dc020fe0c6094a5333cfeaea599a1a89e6
Tiago Costa <ntc132@cmdadmins-mbp.localdomain>
2024-08-23 17:31:33 +0200

    added new grid files (forgotten in previous commit)

---
+++ src/Grid.cpp
@@ -0,0 +1,52 @@
+/*
+ * Grid.cpp
+ *
+ *  Created on: 20 Aug 2024
+ *      Author: ntc132
+ */
+
+#include "SimulationDependencies.h"
+
+Grid::Grid(double xmin, double xmax, double ymin, double ymax, double zmin, double zmax, int nGhost, int nCells) : xmin(xmin), xmax(xmax), ymin(ymin), ymax(ymax), zmin(zmin), zmax(zmax), nGhost(nGhost), nCells(nCells) {
+
+	quantities =  std::vector<std::vector<std::vector<std::vector<int> > > > (nCells + 2*nGhost,
+			std::vector<std::vector<std::vector<int> > >(nCells + 2*nGhost,
+			std::vector<std::vector<int> >(nCells + 2*nGhost,
+			std::vector<int>(nvar, 1.e-2))));
+
+	dx = (xmax - xmin)/nCells;
+	dy = (ymax - ymin)/nCells;
+	dz = (zmax - zmin)/nCells;
+
+	minXIndex = nGhost;
+	maxXIndex = nGhost + nCells - 1;
+
+	minYIndex = nGhost;
+	maxYIndex = nGhost + nCells - 1;
+
+	minZIndex = nGhost;
+	maxZIndex = nGhost + nCells - 1;
+
+	for(int i = minXIndex; i < maxXIndex; i++){
+		double x = getX(i) - 0.5;
+
+		for(int j = minYIndex; j < maxYIndex; j++){
+			double y = getX(j) - 0.5;
+
+			for(int k = minZIndex; k < maxZIndex; k++){
+				double z = getX(k) - 0.5;
+
+				double radius = std::sqrt(x*x + y*y);
+
+				if((radius < 0.4) && (std::fabs(z) < 0.1))
+					quantities[i][j][k][DENS] = 3.e2;
+
+			}
+		}
+	}
+
+}
+
+Grid::~Grid() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/Grid.h
@@ -0,0 +1,52 @@
+/*
+ * Grid.h
+ *
+ *  Created on: 20 Aug 2024
+ *      Author: ntc132
+ */
+
+#include "SimulationDependencies.h"
+
+#ifndef SRC_GRID_H_
+#define SRC_GRID_H_
+
+class Grid {
+public:
+	Grid(double xmin, double xmax, double ymin, double ymax, double zmin, double zmax, int nGhost, int nCells);
+	virtual ~Grid();
+
+	double dx, dy, dz;
+	double xmin, xmax;
+	double ymin, ymax;
+	double zmin, zmax;
+
+	int minXIndex, maxXIndex;
+	int minYIndex, maxYIndex;
+	int minZIndex, maxZIndex;
+
+	int nGhost, nCells;
+
+	int nvar = 2;
+
+	std::vector<std::vector<std::vector<std::vector<int> > > > quantities;
+
+	static const int DENS   = 0;
+	static const int TEMP   = 1;
+
+	std::array<int, 3> getCellIndex(std::vector<double> position){
+		std::array<int, 3> indices;
+
+		indices[0] = minXIndex + std::floor((position[0] - xmin)/dx);
+		indices[1] = minYIndex + std::floor((position[1] - ymin)/dy);
+		indices[2] = minZIndex + std::floor((position[2] - zmin)/dz);
+
+		return indices;
+	}
+
+	double getX(int cellIndex){
+		return xmin + (cellIndex - minXIndex) * dx + dx/2.;
+	}
+};
+
+#endif /* SRC_GRID_H_ */
+
